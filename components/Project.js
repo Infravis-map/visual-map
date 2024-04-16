762f33c4f91335481010820e4addf118792019bb
Adam <adam.egnell@hotmail.com>
2024-04-16 14:32:14 +0200

    första utkast till hemsidan på test.js

---
+++ components/Project.js
@@ -0,0 +1,16 @@
+
+
+export default function Project() {
+    return (
+        <div style={{width:"100%", padding:0,}}>
+            <div style={{padding:10, backgroundColor:"white", borderTopLeftRadius:10, borderTopRightRadius:10}}>
+                <h2 style={{margin:0}}>title</h2>
+                <h6 style={{margin:0, marginTop:5}}>Infravis Node:</h6>
+                <p style={{margin:0}}>KTH</p>
+                <h6 style={{margin:0, marginTop:5}}>Infravis Node Coordinator:</h6>
+                <p style={{margin:0}}>Mario</p>
+            </div>
+            <div style={{height:100, backgroundColor:"lightgray", borderBottomLeftRadius:10, borderBottomRightRadius:10}}></div>
+        </div>
+    )
+}
---
+++ pages/test.js
@@ -0,0 +1,25 @@
+import D3Example from '../components/D3Example'
+import Project from '../components/Project'
+
+export default function TestPage() {
+    return (
+        <div style={{display:"grid", gridTemplateColumns:"1fr 1fr"}}>
+            <div style={{padding:20, paddingRight:0}}>
+                <div style={{width:"100%", marginBottom:20, gap: 10, display:"grid", gridTemplateColumns: "1fr auto auto"}}> 
+                <input placeholder="Search..." type="search" style={{width:"100%", height:40, borderRadius:20, borderStyle:"solid", borderWidth:5, borderColor:"#DE1690", paddingLeft:10, paddingRight:10,}}></input>
+                    <button style={{width:40,height:40, borderStyle:"none", borderRadius:20, backgroundColor:"gray",}}></button>
+                    <button style={{width:40,height:40, borderStyle:"none", borderRadius:20, backgroundColor:"gray",}}></button>
+                </div>
+                <div style={{borderRadius:20, backgroundColor:"gray", padding:15, gap:15, display: "grid", gridTemplateColumns:"1fr 1fr",}}>
+                    <Project/>
+                    <Project/>
+                    <Project/>
+                    <Project/>
+                    <Project/>
+                </div>
+            </div>
+
+            <D3Example width="200" height="200" />
+        </div>
+    )
+}
