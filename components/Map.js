97a8992fdd6b39cb139639b936c2888744b4e6f9
muryuryu <ToshiBruh>
2024-04-29 13:48:00 +0200

    added amrkers and fixed move in map

--- components/Map.js
+++ components/Map.js
@@ -1,19 +1,71 @@
 import { useEffect, useState, useRef, createRef } from "react";
-import { MapContainer, ImageOverlay, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  ImageOverlay,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
 import { DivIcon, LatLngExpression, Layer } from "leaflet";
 import "leaflet/dist/leaflet.css";
+
+const svgIcon = L.divIcon({
+  className: "custom-div-icon",
+  html: `
+  <svg xmlns='http://www.w3.org/2000/svg'
+    width='10'
+    height='10'
+    viewBox='0 0 24 24'
+    fill='#DE1690'
+    stroke-width='0'
+    stroke-linecap='round'
+    stroke-linejoin='round'>
+    <circle
+      cx='12'
+      cy='12'
+      r='10'
+    />
+  </svg>`,
+  iconSize: [10, 10], // size of the icon
+  iconAnchor: [22, 70], // point of the icon which will correspond to marker's location
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
 
 export default function Map() {
   return (
     <MapContainer
-        center={[0, 0]}
-        minZoom={9}
-        zoom={9}
-        maxZoom={12}
-        attributionControl={false}
-        style={{height: '100vh', width: '100%'}}
+      center={[0, 0]}
+      minZoom={9}
+      zoom={9}
+      maxZoom={12}
+      attributionControl={false}
+      style={{ height: "100vh", width: "100%" }}
+      maxBounds={[
+        [-1, -1],
+        [1, 1],
+      ]} // Limit panning to these bounds
+      maxBoundsViscosity={0.95} // How solid the bounds are when user tries to pan outside
     >
-        <ImageOverlay url="se.svg" bounds={[[-1, -1], [1, 1]]}/>
+      <ImageOverlay
+        url="se.svg"
+        bounds={[
+          [-1, -1],
+          [1, 1],
+        ]}
+      />
+      <Marker position={[0, 0.2]} icon={svgIcon}>
+        <Popup>ok. helo</Popup>
+      </Marker>
+      <Marker position={[0, 0]} icon={svgIcon}>
+        <Popup>ok. helo</Popup>
+      </Marker>
+      <Marker position={[0, 0]} icon={svgIcon}>
+        <Popup>ok. helo</Popup>
+      </Marker>
+      <Marker position={[0, 0]} icon={svgIcon}>
+        <Popup>ok. helo</Popup>
+      </Marker>
+      
     </MapContainer>
   );
 }
