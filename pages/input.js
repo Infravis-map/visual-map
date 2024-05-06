231f5cf6ac1f1db07415b455c14a5012eae5d76f
Adam <adam.egnell@hotmail.com>
2024-05-06 11:44:45 +0200

    title and about input

--- pages/input.js
+++ pages/input.js
@@ -1,9 +1,29 @@
+import { useState } from "react"
+import styles from '../styles/input.module.css'
 
+export default function InputPage() {
+    
+    const [title, setTitle] = useState("")
+    const [about, setAbout] = useState("")
 
-export default function TestPage() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`title: ${title}`)
+    }
+
     return (
-        <div>
-            
+        <div style={{display:"flex", justifyContent:"center"}}>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <label>
+                    Title
+                    <input type="text" value={title} name="title" onChange={(e) => setTitle(e.target.value)} placeholder="Title"></input>
+                </label>
+                <label>
+                    About
+                    <p>roughly 150 words</p>
+                    <input type="text" value={about} name="about" onChange={(e) => setAbout(e.target.value)} placeholder="About"></input>
+                </label>
+            </form>
         </div>
     )
 }
