f8e07728bb11d4ef0f9a407ad3f544f7758eb959
Adam <adam.egnell@hotmail.com>
2024-05-18 16:14:26 +0200

    grey subtitle in Project.js

--- components/Project.js
+++ components/Project.js
@@ -15,18 +15,18 @@ export default function Project({title, nodes, coordinator, firstImage}) {
     return (
         <div style={{width:"100%", padding:0}} onClick={() => setShowEdit(!showEdit)}>
             <div style={{padding:10, backgroundColor:"white", borderTopLeftRadius:10, borderTopRightRadius:10, borderBottomLeftRadius:containerBorderRadius, borderBottomRightRadius:containerBorderRadius}}>
-                <h2 style={{margin:0}}>{title}</h2>
+                <h2 style={{margin:0, marginBottom:7}}>{title}</h2>
 
                 {nodes.length >= 1 ? 
                     <div>
-                        <h6 style={{margin:0, marginTop:5}}>Infravis Node:</h6>
+                        <h6 style={{margin:0, marginTop:5, color:"grey"}}>Infravis Node:</h6>
                         <p style={{margin:0}}>{nodes.join(", ")}</p>
                     </div>
                 : null}
 
                 {coordinator ? 
                     <div>
-                        <h6 style={{margin:0, marginTop:5}}>Infravis Node Coordinator:</h6>
+                        <h6 style={{margin:0, marginTop:5, color:"grey"}}>Infravis Node Coordinator:</h6>
                         <p style={{margin:0}}>{coordinator}</p>
                     </div>
                 : null}
